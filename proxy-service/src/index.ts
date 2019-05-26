6776064081d2b89bc423144f86322a15800534b1
Piotr Walen <walen.piotr@gmail.com>
2019-05-26 10:01:08 +0200

    Add proxy service

---
+++ proxy-service/src/index.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+import Express from "express";
+import httpProxy from "express-http-proxy";
+
+// const proxy = (host: string, root: string) =>
+//   httpProxy(host, {
+//     proxyReqPathResolver: req => `${root}${req.url}`,
+//   });
+
+const main = async () => {
+  const { AUTH_HOST, APP_HOST, PORT } = process.env;
+  if (!AUTH_HOST) {
+    throw Error("No AUTH_HOST env var provided");
+  }
+  if (!APP_HOST) {
+    throw Error("No APP_HOST env var provided");
+  }
+  if (!PORT) {
+    throw Error("No PORT env var provided");
+  }
+  const app = Express();
+  app.get("/isalive", (_, res) => res.send("Proxy service is alive"));
+  app.use("/auth", httpProxy(AUTH_HOST));
+  // app.use("/", httpProxy(APP_HOST));
+  app.listen(PORT, () => {
+    console.log(`Proxy service started on http://localhost:${PORT}`);
+  });
+};
+
+main().catch(err => console.error(err));
