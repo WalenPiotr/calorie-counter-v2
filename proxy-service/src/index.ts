7310e0978be982d408b91016de6de7f2e27facfb
Piotr Walen <walen.piotr@gmail.com>
2019-05-27 10:50:31 +0200

    Add resource service to proxy service

--- proxy-service/src/index.ts
+++ proxy-service/src/index.ts
@@ -5,13 +5,8 @@ if (process.env.NODE_ENV !== "production") {
 import Express from "express";
 import httpProxy from "express-http-proxy";
 
-// const proxy = (host: string, root: string) =>
-//   httpProxy(host, {
-//     proxyReqPathResolver: req => `${root}${req.url}`,
-//   });
-
 const main = async () => {
-  const { AUTH_HOST, APP_HOST, PORT } = process.env;
+  const { AUTH_HOST, APP_HOST, PORT, API_HOST } = process.env;
   if (!AUTH_HOST) {
     throw Error("No AUTH_HOST env var provided");
   }
@@ -21,9 +16,13 @@ const main = async () => {
   if (!PORT) {
     throw Error("No PORT env var provided");
   }
+  if (!API_HOST) {
+    throw Error("No API_HOST env var provided");
+  }
   const app = Express();
   app.get("/isalive", (_, res) => res.send("Proxy service is alive"));
   app.use("/auth", httpProxy(AUTH_HOST));
+  app.use("/api", httpProxy(API_HOST));
   // app.use("/", httpProxy(APP_HOST));
   app.listen(PORT, () => {
     console.log(`Proxy service started on http://localhost:${PORT}`);
