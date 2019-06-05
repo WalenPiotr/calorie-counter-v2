d9aae083252cdee90e070cb9d4d999bd7a9c9a96
Piotr Walen <walen.piotr@gmail.com>
2019-06-05 11:11:41 +0200

    Add status for user

--- resource-service/src/helpers/authChecker.ts
+++ resource-service/src/helpers/authChecker.ts
@@ -6,16 +6,27 @@ export enum Role {
   ADMIN = "ADMIN",
 }
 
+export enum Status {
+  OK = "OK",
+  BANNED = "BANNED",
+}
+
 export const authChecker: AuthChecker<ContextType> = ({ context }, roles) => {
   const { session } = context.req;
   if (roles.length === 0) {
-    return session && session.passport && session.passport.user;
+    return (
+      session &&
+      session.passport &&
+      session.passport.user &&
+      session.passport.user.status === Status.OK
+    );
   } else {
     return (
       session &&
       session.passport &&
       session.passport.user &&
-      roles.indexOf(session.passport.user.role) >= 0
+      roles.indexOf(session.passport.user.role) >= 0 &&
+      session.passport.user.status === Status.OK
     );
   }
 };
