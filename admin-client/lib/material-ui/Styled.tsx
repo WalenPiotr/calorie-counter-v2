cafd4f4472f207de93baa0d684df431e09addd6d
Piotr Walen <walen.piotr@gmail.com>
2019-06-06 12:12:49 +0200

    Add material ui and apollo support

---
+++ admin-client/lib/material-ui/Styled.tsx
@@ -0,0 +1,35 @@
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import withStyles, {
+    StyleRules,
+    StyleRulesCallback,
+    WithStyles,
+    WithStylesOptions,
+} from "@material-ui/core/styles/withStyles";
+import * as React from "react";
+
+interface StyledProps<
+    T extends string | StyleRules | StyleRulesCallback = string
+> {
+    children: (props: WithStyles<T, true>) => React.ReactNode;
+}
+interface InjectedProps {
+    classes: any;
+    theme: any;
+}
+
+function createStyled<ClassKey extends string>(
+    styles: StyleRulesCallback<ClassKey> | StyleRules<ClassKey>,
+): React.ComponentType<StyledProps<typeof styles>> {
+    class Styled extends React.Component<
+        StyledProps<typeof styles> & InjectedProps
+    > {
+        render() {
+            return this.props.children({
+                classes: this.props.classes,
+                theme: this.props.theme,
+            });
+        }
+    }
+    return withStyles(styles, { withTheme: true })(Styled);
+}
+export default createStyled;
--- admin-client/pages/index.tsx
+++ admin-client/pages/index.tsx
@@ -1,17 +1,40 @@
 import React from "react";
 import Link from "next/link";
+import Button from "@material-ui/core/Button";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import createStyled from "../lib/material-ui/Styled";
+
+const Styled = createStyled((theme: Theme) => ({
+  button: {
+    margin: theme.spacing(2),
+  },
+}));
 
 export default () => (
-  <ul>
-    <li>
-      <Link href="/a" as="/a">
-        <a>a</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/b" as="/b">
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li>
+        <Link href="/a" as="/a">
+          <a>a</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/b" as="/b">
+          <a>b</a>
+        </Link>
+      </li>
+    </ul>
+    <Styled>
+      {({ classes }) => (
+        <Button
+          variant="outlined"
+          color="secondary"
+          size="large"
+          className={classes.button}
+        >
+          Click me
+        </Button>
+      )}
+    </Styled>
+  </div>
 );
