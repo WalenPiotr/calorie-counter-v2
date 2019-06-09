6a158befe17475055f1ff16cdbb1b8486fada6cd
Piotr Walen <walen.piotr@gmail.com>
2019-06-09 17:03:36 +0200

    Add api-gateway schema generator script

---
+++ api-gateway/src/server.ts
@@ -0,0 +1,19 @@
+import { createHttpLink } from "apollo-link-http";
+import { introspectSchema } from "graphql-tools";
+import fs from "fs";
+import path from "path";
+import { printSchema } from "graphql/utilities";
+import "cross-fetch/polyfill";
+
+const destination = "src/data";
+const httpConvectionLink = createHttpLink({
+  fetch,
+  uri: "http://localhost:8080/auth/graphql",
+});
+
+introspectSchema(httpConvectionLink).then(schema => {
+  fs.writeFileSync(
+    path.join(destination, "auth-api.graphql"),
+    printSchema(schema, { commentDescriptions: true }),
+  );
+});
