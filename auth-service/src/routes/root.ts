cdfac1c14ffc141993f27e7a318121ca4805eef7
Piotr Walen <walen.piotr@gmail.com>
2019-06-11 19:36:19 +0200

    Add redirect after logout

--- auth-service/src/routes/root.ts
+++ auth-service/src/routes/root.ts
@@ -2,10 +2,15 @@ import Express from "express";
 
 const router = Express.Router();
 
+const { SUCCESS_REDIRECT_URL } = process.env;
+if (!SUCCESS_REDIRECT_URL) {
+  throw Error("No SUCCESS_REDIRECT_URL env var");
+}
+
 router.get("/logout", (req, res) => {
   if (req.session) {
     req.session.destroy(() => {
-      res.send("successfully log out!");
+      res.redirect(SUCCESS_REDIRECT_URL);
     });
   }
 });
